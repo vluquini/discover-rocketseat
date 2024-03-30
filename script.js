cdd61b7cf503b65319327c3aa2cf9d62c72cb76f
Vinicius Nunes <theviniciusluquini@gmail.com>
2024-03-29 22:21:34 -0300

    modulo 5: aula 7 - finalizando logica de troca de imagem

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function toggleMode() {
   const html = document.documentElement
-  // lista de classe contidas no documento html
-  // if(html.classList.contains('light')){
-  //   html.classList.remove('light')
-  // }else
-  //   html.classList.add('light')
-
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  // lista de classe contidas no documento html
+  if(html.classList.contains('light')){
+    img.setAttribute("src", "./assets/black2.jpg")
+  }else{
+    img.setAttribute("src", "./assets/Chibi cyberpunk Code breaker - bola.jpg")
+  }
+
+  
 }
