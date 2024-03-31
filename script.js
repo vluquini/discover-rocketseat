b3d92339dbc1b30db5b2c4374a9564d9f89393da
Vinicius Nunes <theviniciusluquini@gmail.com>
2024-03-30 21:50:43 -0300

    modulo 6: aula 5 - evoluindo projeto para outros dispositivos

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-
   const img = document.querySelector("#profile img")
   // lista de classe contidas no documento html
   if(html.classList.contains('light')){
@@ -9,6 +8,5 @@ function toggleMode() {
   }else{
     img.setAttribute("src", "./assets/Chibi cyberpunk Code breaker - bola.jpg")
   }
-
   
 }
